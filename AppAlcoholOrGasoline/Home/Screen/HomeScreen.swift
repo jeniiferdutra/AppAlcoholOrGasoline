a90692715fb9274bcc25d1dc258a51986fbacab0
jenifer rocha <jeniferdutra2000@icloud.com>
2024-11-04 20:36:28 -0300

    adicionado a logo e botao

--- AppAlcoholOrGasoline/Home/Screen/HomeScreen.swift
+++ AppAlcoholOrGasoline/Home/Screen/HomeScreen.swift
@@ -13,13 +13,35 @@ class HomeScreen: UIView {
         let img = UIImageView()
         img.translatesAutoresizingMaskIntoConstraints = false
         img.image = UIImage(named: "BG HOME")
-        img.contentMode = .scaleAspectFill // Preenche o espaço sem distorcer, mas corta as bordas
+        img.contentMode = .scaleAspectFit // Ajusta a imagem para caber na view, mantendo a proporção.
         return img
+    }()
+    
+    lazy var logoAppImageView: UIImageView = {
+        let img = UIImageView()
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.image = UIImage(named: "LOGO")
+        img.contentMode = .scaleAspectFit
+       return img
+    }()
+    
+    lazy var startButton: UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setTitle("Começar", for: .normal)
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        btn.setTitleColor(.white, for: .normal)
+        btn.clipsToBounds = true
+        btn.layer.cornerRadius = 8
+        btn.backgroundColor = UIColor(red: 230/255, green: 0/255, blue: 127/255, alpha: 1.0)
+        return btn
     }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(backgroundImage)
+        addSubview(logoAppImageView)
+        addSubview(startButton)
         configConstraints()
     }
     
@@ -33,7 +55,16 @@ class HomeScreen: UIView {
             backgroundImage.topAnchor.constraint(equalTo: topAnchor),
             backgroundImage.leadingAnchor.constraint(equalTo: leadingAnchor),
             backgroundImage.trailingAnchor.constraint(equalTo: trailingAnchor),
-            backgroundImage.bottomAnchor.constraint(equalTo: bottomAnchor)
+            backgroundImage.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            logoAppImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            logoAppImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            logoAppImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            
+            startButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -125),
+            startButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 60),
+            startButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -60),
+            startButton.heightAnchor.constraint(equalToConstant: 44)
             
         ])
     }
