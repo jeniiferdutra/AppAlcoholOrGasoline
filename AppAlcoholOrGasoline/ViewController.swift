6b34f7c6ff27750a57e90d1e70c0e8fff19602c6
jenifer rocha <jeniferdutra2000@icloud.com>
2024-11-04 19:28:07 -0300

    configuracoes iniciais do app

---
+++ AppAlcoholOrGasoline/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppAlcoholOrGasoline
+//
+//  Created by Jenifer Rocha on 04/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
