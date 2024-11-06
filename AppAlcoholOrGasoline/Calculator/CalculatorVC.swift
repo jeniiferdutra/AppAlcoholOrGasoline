46d891ac25d40efac33622b3c9b791a295922c54
jenifer rocha <jeniferdutra2000@icloud.com>
2024-11-06 20:47:47 -0300

    criando logica para calculo do preço do combustivel

--- AppAlcoholOrGasoline/Calculator/CalculatorVC.swift
+++ AppAlcoholOrGasoline/Calculator/CalculatorVC.swift
@@ -25,11 +25,24 @@ class CalculatorVC: UIViewController {
 
 extension CalculatorVC: CalculatorScreenDelegate {
     func tappedCalculateButton() {
-        print(#function) // printar o nome da propria funcao
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        
+        let ethanolPrice: Double = (formatter.number(from: screen?.ethanolPriceTextField.text ?? "0.0") as? Double) ?? 0.0
+        
+        let gasPrice: Double = (formatter.number(from: screen?.gasPriceTextField.text ?? "0.0") as? Double) ?? 0.0
+        
+        if ethanolPrice / gasPrice > 0.7 {
+            print("Melhor utilizar gasolina")
+        }
+        else {
+            print("Melhor utilizar álcool")
+        }
     }
     
     func tappedBackButton() {
-        print(#function)
+        navigationController?.popViewController(animated: true)
     }
     
     
