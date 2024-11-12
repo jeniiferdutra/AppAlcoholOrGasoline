2e17f73a05dd0f905d9422554eae9d74dc0dc8f0
jenifer rocha <jeniferdutra2000@icloud.com>
2024-11-11 22:12:43 -0300

    criando elementos para tela de resultado

--- AppAlcoholOrGasoline/Result/Screen/ResultScreen.swift
+++ AppAlcoholOrGasoline/Result/Screen/ResultScreen.swift
@@ -34,12 +34,45 @@ class ResultScreen: UIView {
         label.text = "Abasteça com:"
         return label
     }()
+    
+    lazy var resultLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 73)
+        label.text = "Gasolina"
+        return label
+    }()
+    
+    lazy var calculateButton: UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setTitle("Calcular Novamente", for: .normal)
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        btn.setTitleColor(.white, for: .normal)
+        btn.clipsToBounds = true
+        btn.layer.cornerRadius = 8
+        btn.backgroundColor = UIColor(red: 230/255, green: 0/255, blue: 127/255, alpha: 1.0)
+        //btn.target(forAction: <#T##Selector#>, withSender: <#T##Any?#>)
+        return btn
+    }()
+    
+    lazy var backButton: UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setImage(UIImage(named: "Botão Back"), for: .normal)
+        //btn.target(forAction:  , withSender: <#T##Any?#>)
+        return btn
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(backgroundImageView)
         addSubview(logoAppImageView)
         addSubview(sentenceLabel)
+        addSubview(resultLabel)
+        addSubview(calculateButton)
+        addSubview(backButton)
         configConstraints()
     }
     
@@ -54,12 +87,24 @@ class ResultScreen: UIView {
             backgroundImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
             backgroundImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
             
+            backButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            backButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            
             logoAppImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 60),
             logoAppImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
             logoAppImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
             
             sentenceLabel.topAnchor.constraint(equalTo: logoAppImageView.bottomAnchor, constant: 180),
-            sentenceLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+            sentenceLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            resultLabel.topAnchor.constraint(equalTo: sentenceLabel.bottomAnchor, constant: 12),
+            resultLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            calculateButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -135),
+            calculateButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 60),
+            calculateButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -60),
+            calculateButton.heightAnchor.constraint(equalToConstant: 44),
+            
         ])
     }
 }
