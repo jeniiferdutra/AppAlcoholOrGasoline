4737eb41ac8e49f7c97ecd0f8c8140abbb3abafb
jenifer rocha <jeniferdutra2000@icloud.com>
2024-11-20 19:21:55 -0300

    adicionado framework IQKeyboardManagerSwift no projeto

--- AppAlcoholOrGasoline/AppDelegate.swift
+++ AppAlcoholOrGasoline/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Habilita o IQKeyboardManager para gerenciar automaticamente o comportamento do teclado.
+        // Quando ativado, ele move os campos de texto para cima quando o teclado aparece,
+        // evitando que o conteúdo da tela fique oculto.
+        IQKeyboardManager.shared.isEnabled = true
         return true
     }
 
---
+++ Pods/Target Support Files/Pods-AppAlcoholOrGasoline/Pods-AppAlcoholOrGasoline-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppAlcoholOrGasoline : NSObject
+@end
+@implementation PodsDummy_Pods_AppAlcoholOrGasoline
+@end
