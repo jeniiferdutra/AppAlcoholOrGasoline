2eb585bf2cbf632e2a727401075aa386d8ed5f9a
jenifer rocha <jeniferdutra2000@icloud.com>
2024-11-20 15:30:18 -0300

    criando action para tela de resultado

--- AppAlcoholOrGasoline/Result/ResultVC.swift
+++ AppAlcoholOrGasoline/Result/ResultVC.swift
@@ -7,9 +7,24 @@
 
 import UIKit
 
+enum BestFuel: String {
+    case gas = "Gasolina"
+    case ethanol = "Álcool"
+}
+
 class ResultVC: UIViewController {
     
     var screen: ResultScreen?
+    let bestFuel: BestFuel
+    
+    init(bestFuel: BestFuel) {
+        self.bestFuel = bestFuel
+        super.init(nibName: nil, bundle: nil) // Inicializa o UIViewController sem usar um arquivo XIB
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func loadView() {
         screen = ResultScreen()
@@ -19,6 +34,7 @@ class ResultVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         screen?.delegate(delegate: self)
+        screen?.resultLabel.text = bestFuel.rawValue // Exibe no label o valor textual da enum representando o melhor combustível
     }
     
     func popViewController() {
